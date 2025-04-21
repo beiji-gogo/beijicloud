0b69daf5d28d8e268a8c2378d3283203bce3efdc
beiji-gogo <Mlim4450@gmail.com>
2025-04-21 12:17:47 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://panel.44s4.vip/'
+window.settings = {
+  // 站点名称
+  title: '北极云',
+  // 站点描述
+  description: '北极云',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'blue', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
